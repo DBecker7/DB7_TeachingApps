977ac94eb1b75769f19984b78ba8cc3a4e2bd3b4
DBecker7 <d7becker@gmail.com>
2020-04-28 11:19:35 -0400

    Adding the App

---
+++ PoisBinApprox/app.R
@@ -0,0 +1,96 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above (RStudio Only - otherwise, just run the whole 
+# script).
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+# Make sure these packages are installed first.
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Poisson Approximation to the Binomial Distribution"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("n",
+                        "Sample Size",
+                        min = 5,
+                        max = 5000,
+                        step = 5,
+                        value = 30, animate = list(interval = 900)),
+            "The probability slider is on the log scale so it's easier to select small probabilities. The actual probability is shown in the plot title.",
+            sliderInput("p",
+                        "log10(Probability)",
+                        min = -5,
+                        max = -0.05,
+                        step = 0.05,
+                        value = -1, animate = list(interval = 900)),
+            "Pro Tip: There's a 'play' button on the slider!",
+            checkboxInput("addnorm", "Add the Normal approximation?")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot", height = "600px")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        n <- input$n
+        p <- 10^(input$p)
+        xseq <- seq(0, n, 1)
+        plotlims <- c(max(0, floor(n*p - 4*sqrt(n*p*(1-p)))), 
+            min(n, ceiling(n*p + 4*sqrt(n*p*(1-p)))))
+        bindf <- data.frame(x = xseq, y = dbinom(xseq, n, p), 
+            Model = "Binomial", 
+            stringsAsFactors = FALSE)
+        poisdf <- data.frame(x = xseq, y = dpois(xseq, n*p), 
+            Model = "Poisson", 
+            stringsAsFactors = FALSE)
+        normdf <- data.frame(x = xseq, y = dnorm(xseq, n*p, sqrt(n*p*(1-p))), 
+            Model = "Normal", 
+            stringsAsFactors = FALSE)
+        ggdf <- bind_rows(bindf, poisdf)
+        
+        normmse <- sum((bindf$y - normdf$y)^2)
+        poismse <- sum((bindf$y - poisdf$y)^2)
+        mytitle <- paste0("Sum of Squared Error (Poisson) = ", 
+            round(poismse, 7))
+        if(input$addnorm) mytitle <- paste0(mytitle, 
+            "; SSE(Normal) = ", round(normmse, 7))
+        
+        ggplot() + 
+            geom_col(data = ggdf, 
+                mapping = aes(x = x, y = y, fill = Model, colour = Model),
+                position = "identity", alpha = 0.5) +
+            scale_fill_manual(values = c("#7570b3", "#66a61e")) +
+            scale_colour_manual(values = c("#7570b3", "#66a61e")) +
+            theme_bw() +
+            coord_cartesian(xlim = plotlims) +
+            labs(x = "x", y = "f(x)", 
+                title = mytitle,
+                subtitle = paste0("n = ", n, ", p = ", round(p, 7))) +
+            theme(legend.position = "bottom") +
+            if(input$addnorm) stat_function(aes(x = xseq),
+                fun = function(x) dnorm(x, n*p, sqrt(n*p*(1-p))), 
+                n = 1000, xlim = plotlims)
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
