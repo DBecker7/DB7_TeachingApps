b8b8991a3d6de3bf2ec07173442441f124a60eea
DBecker7 <d7becker@gmail.com>
2020-05-03 15:41:22 -0400

    Changed front matter

--- SpatialFun/GausProcess_Matern/app.R
+++ SpatialFun/GausProcess_Matern/app.R
@@ -1,11 +1,5 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# GaussProcess_Matern
+# Sliders for changing the parameters to see how the Matern process changes
 
 myseed <- 2112
 library(ggplot2)
