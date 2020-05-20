29ce5632fcdae61d626de76ee059167a0cfac3f2
DBecker7 <d7becker@gmail.com>
2020-05-20 09:33:50 -0400

    Annotations rather than scale_x

--- Tools/pnorm/app.R
+++ Tools/pnorm/app.R
@@ -36,20 +36,25 @@ server <- function(input, output) {
         xrib <- seq(lo, hi, 0.01)
         yrib <- dnorm(xrib)
         
-        mytitle <- if(lo > -4 & hi < 4){
-            paste0("pnorm(", hi, ") - pnorm(",lo,") = ", round(pnorm(hi) - pnorm(lo), 4))
+        if(lo > -4 & hi < 4){
+            mytitle <- paste0("pnorm(", hi, ") - pnorm(",lo,") = ", round(pnorm(hi) - pnorm(lo), 4))
+            addtox <- c(lo, hi)
         } else if(hi < 4){
-            paste0("pnorm(", hi, ") = ", round(pnorm(hi), 4))
+            mytitle <- paste0("pnorm(", hi, ") = ", round(pnorm(hi), 4))
+            addtox <- hi
         } else {
-            paste0("1 - pnorm(", lo, ") = ", round(1 - pnorm(lo), 4))
+            mytitle <- paste0("1 - pnorm(", lo, ") = ", round(1 - pnorm(lo), 4))
+            addtox <- lo
         }
         
         ggplot() + 
             geom_ribbon(aes(xmin = xrib, x=xrib, ymin = 0, ymax = yrib),
                 fill = "darkorchid", alpha = 0.4) +
             geom_line(aes(x = xseq, y = yseq), size = 1) +
-            scale_x_continuous(breaks = c(seq(-4,4,2), lo, hi)) +
+            scale_x_continuous(breaks = c(seq(-4,4,2))) +
             theme_bw() + 
+            annotate("text", x = addtox, y = rep(-0.01, length(addtox)), 
+                label = addtox) + 
             labs(x = "x", y = "dnorm(x)", title = mytitle,
                 caption = "Created by Devan Becker\nGithub: DBecker7/DB7_TeachingApps") + 
             theme(title = element_text(size = 16), 
