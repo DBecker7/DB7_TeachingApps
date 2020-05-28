8c4be7fb38c47ee7c211d481050728d07c7743f1
DBecker7 <d7becker@gmail.com>
2020-05-28 09:24:14 -0400

    Changed steps, added weibull

--- Tools/distrshapes/app.R
+++ Tools/distrshapes/app.R
@@ -5,7 +5,7 @@ parameter_tabs <- tagList(
     tabsetPanel(id = "params",
         tabPanel("normal",
             sliderInput("mean", "mean", min = -5, max = 5, 
-                value = 1, step = 0.01,
+                value = 1, step = 0.05,
                 animate = list(interval = 600)),
             sliderInput("sd", "standard deviation", min = 0.01, max = 10, 
                 value = 1, step = 0.01,
@@ -25,19 +25,27 @@ parameter_tabs <- tagList(
                 animate = list(interval = 600))
         ),
         tabPanel("gamma",
-            sliderInput("gshape", "shape", min = 0.01, max = 20, 
-                value = 1, step = 0.01,
+            sliderInput("gshape", "shape", min = 0.05, max = 20, 
+                value = 1, step = 0.05,
                 animate = list(interval = 600)),
-            sliderInput("grate", "rate",  min = 0.01, max = 20, 
-                value = 1, step = 0.01,
+            sliderInput("grate", "rate",  min = 0.05, max = 20, 
+                value = 1, step = 0.05,
                 animate = list(interval = 600))
         ),
         tabPanel("beta",
             sliderInput("bshape1", "shape 1", min = 0.01, max = 20, 
-                value = 1, step = 0.01,
+                value = 1, step = 0.05,
                 animate = list(interval = 600)),
-            sliderInput("bshape2", "shape 2",  min = 0.01, max = 20, 
-                value = 1, step = 0.01,
+            sliderInput("bshape2", "shape 2",  min = 0.05, max = 20, 
+                value = 1, step = 0.05,
+                animate = list(interval = 600))
+        ),
+        tabPanel("weibull",
+            sliderInput("wshape", "shape", min = 0.05, max = 20, 
+                value = 1, step = 0.05,
+                animate = list(interval = 600)),
+            sliderInput("wscale", "scale",  min = 0.05, max = 20, 
+                value = 1, step = 0.05,
                 animate = list(interval = 600))
         )
     )
@@ -50,10 +58,10 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             selectInput("dist", "Distribution", 
-                choices = c("normal", "uniform", "exponential", "gamma", "beta")
+                choices = c("normal", "uniform", "exponential", "gamma", "beta", "weibull")
             ),
             numericInput("n", "Number of samples", value = 100),
-            sliderInput("bins", "binwidth", min = 0.1, max = 5, value = 1, step = 0.1),
+            sliderInput("bins", "binwidth", min = 0.05, max = 5, value = 1, step = 0.05),
             parameter_tabs,
             actionButton("doit", "Click me for new data"),
             actionButton("axes", "Reset axes and ghosts")
@@ -88,7 +96,8 @@ server <- function(input, output, session) {
             uniform = runif(input$n, input$min, input$max),
             exponential = rexp(input$n, input$rate),
             gamma = rgamma(input$n, shape = input$gshape, rate = input$grate),
-            beta = rbeta(input$n, shape1 = input$bshape1, shape2 = input$bshape2)
+            beta = rbeta(input$n, shape1 = input$bshape1, shape2 = input$bshape2),
+            weibull = rweibull(input$n, shape = input$wshape, scale = input$wscale)
         )
     })
     
@@ -99,10 +108,11 @@ server <- function(input, output, session) {
         lohi <- switch(input$dist,
             normal = c(input$mean - 3*input$sd, input$mean + 3*input$sd),
             uniform = c(min(unifs), max(unifs)),
-            exponential = c(0, 5/input$rate),
+            exponential = c(0, qexp(0.999, input$rate)),
             #gamma = c(0,forgamma[min(which(mydgamma < 0.005))])
-            gamma = c(0, ifelse(input$grate < 0.2, 600, 100)),
-            beta = c(0, 1)
+            gamma = c(0, qgamma(0.999, input$gshape, input$grate)),
+            beta = c(0, 1),
+            weibull = c(0, qweibull(0.999, input$wshape, input$wscale))
         )
         
         xseq <- seq(lohi[1], lohi[2], length.out = 500)
@@ -113,7 +123,8 @@ server <- function(input, output, session) {
             uniform = dunif(xseq, min(unifs), max(unifs)),
             exponential = dexp(xseq, input$rate),
             gamma = dgamma(xseq, shape = input$gshape, rate = input$grate),
-            beta = dbeta(xseq, shape1 = input$bshape1, shape2 = input$bshape2)
+            beta = dbeta(xseq, shape1 = input$bshape1, shape2 = input$bshape2),
+            weibull = dweibull(xseq, shape = input$wshape, scale = input$wscale)
         )
         
        
@@ -153,7 +164,8 @@ server <- function(input, output, session) {
                 uniform = bquote("f(x)="*1/(b-a)),
                 exponential = bquote("f(x)="*lambda*exp(-lambda*x)),
                 gamma = bquote("f(x)="*beta^alpha*x^{alpha-1}*exp(-beta*x)/Gamma(alpha)),
-                beta = bquote("f(x)="*Gamma(alpha*"+"*beta)*x^{alpha-1}*(1-x)^{beta-1}/(Gamma(alpha)*Gamma(beta)))
+                beta = bquote("f(x)="*Gamma(alpha*"+"*beta)*x^{alpha-1}*(1-x)^{beta-1}/(Gamma(alpha)*Gamma(beta))),
+                weibull = bquote("f(x)=")
             ),
             bty = "n", text.col = 4, cex = 1)
         
