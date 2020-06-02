e664d4a23d6d15671f1f7a79efd81df0045a380a
DBecker7 <d7becker@gmail.com>
2020-06-02 19:01:13 -0400

    tidyverse_style, minor fixes

--- Tools/distrshapes_disc/app.R
+++ Tools/distrshapes_disc/app.R
@@ -4,41 +4,41 @@ parameter_tabs <- tagList(
     tags$style("#params { display:none; }"),
     tabsetPanel(id = "params",
         tabPanel("binomial",
-            sliderInput("bsize", "size n", min = 1, max = 100, 
+            sliderInput("bsize", "size n", min = 1, max = 100,
                 value = 4, step = 1,
                 animate = list(interval = 600)),
-            sliderInput("bprob", "prob p", min = 0, max = 1, 
+            sliderInput("bprob", "prob p", min = 0, max = 1,
                 value = 0.5, step = 0.01,
                 animate = list(interval = 600))
         ),
         tabPanel("negbinomial",
-            sliderInput("nbsize", "size n", min = 1, max = 100, 
+            sliderInput("nbsize", "size n", min = 1, max = 100,
                 value = 4, step = 1,
                 animate = list(interval = 600)),
-            sliderInput("nbprob", "prob p", min = 0, max = 1, 
+            sliderInput("nbprob", "prob p", min = 0, max = 1,
                 value = 0.5, step = 0.01,
                 animate = list(interval = 600))
         ),
         tabPanel("geometric",
-            sliderInput("gprob", "prob p", min = 0, max = 1, 
+            sliderInput("gprob", "prob p", min = 0, max = 1,
                 value = 0.5, step = 0.01,
                 animate = list(interval = 600))
         ),
         tabPanel("hypergeometric",
             "X is the number of white marbles",
-            sliderInput("hypm", "white marbles m", min = 0, max = 50, 
+            sliderInput("hypm", "white marbles m", min = 0, max = 50,
                 value = 1, step = 1,
                 animate = list(interval = 600)),
-            sliderInput("hypn", "black marbles n", min = 0, max = 50, 
+            sliderInput("hypn", "black marbles n", min = 0, max = 50,
                 value = 1, step = 1,
                 animate = list(interval = 600)),
             "Nothing is plotted if k > m + n",
-            sliderInput("hypk", "marbles drawn k", min = 0, max = 50, 
+            sliderInput("hypk", "marbles drawn k", min = 0, max = 50,
                 value = 1, step = 1,
                 animate = list(interval = 600))
         ),
         tabPanel("poisson",
-            sliderInput("lambda", "rate λ", min = 0, max = 50, 
+            sliderInput("lambda", "rate <U+03BB>", min = 0, max = 50,
                 value = 1, step = 1,
                 animate = list(interval = 600))
         )
@@ -47,7 +47,7 @@ parameter_tabs <- tagList(
 
 geese <- 1
 myseed <- 1
-setlist <- list(data.frame(x=NA, y = NA, g = 0))
+setlist <- list(data.frame(x = NA, y = NA, g = 0))
 
 ui <- fluidPage(
     tags$style(
@@ -62,7 +62,7 @@ ui <- fluidPage(
     ),
     sidebarLayout(
         sidebarPanel(
-            selectInput("dist", "Distribution", 
+            selectInput("dist", "Distribution",
                 choices = c("binomial", "poisson", "geometric", "negbinomial", "hypergeometric")
             ),
             numericInput("n", "Number of samples", value = 100),
@@ -79,19 +79,19 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     observeEvent(input$dist, {
         updateTabsetPanel(session, "params", selected = input$dist)
-    }) 
-    
+    })
+
     axes <- reactive({
         input$axes
         input$dist
-        
+
         xmin <<- 0
         xmax <<- 0
         ymax <<- 0
-        
-        setlist <<- list(data.frame(x=NA, y = NA, g = 0))
+
+        setlist <<- list(data.frame(x = NA, y = NA, g = 0))
     })
-    
+
     sample <- reactive({
         set.seed(myseed)
         input$doit
@@ -100,14 +100,14 @@ server <- function(input, output, session) {
             negbinomial = rnbinom(input$n, input$nbsize, input$nbprob),
             geometric = rgeom(input$n, input$gprob),
             poisson = rpois(input$n, input$lambda),
-            hypergeometric = rhyper(input$n, input$hypm, 
+            hypergeometric = rhyper(input$n, input$hypm,
                 input$hypn, input$hypk)
         )
     })
-    
+
     distfun <- reactive({
-        
-        
+
+
         lohi <- switch(input$dist,
             binomial = c(0, input$bsize),
             negbinomial = c(0, qnbinom(0.999, input$nbsize, input$nbprob)),
@@ -115,79 +115,79 @@ server <- function(input, output, session) {
             poisson = c(0, qpois(0.999, input$lambda)),
             hypergeometric = c(0, min(input$hypn, input$hypk))
         )
-        
+
         xseq <- seq(lohi[1], lohi[2] + 1, by = 1)
-        
-        
+
+
         yseq <- switch(input$dist,
             binomial = dbinom(xseq, input$bsize, input$bprob),
             negbinomial = dnbinom(xseq, input$nbsize, input$nbprob),
             geometric = dgeom(xseq, input$gprob),
             poisson = dpois(xseq, input$lambda),
-            hypergeometric = dhyper(xseq, input$hypm, 
+            hypergeometric = dhyper(xseq, input$hypm,
                 input$hypn, input$hypk)
         )
-        
-        
+
+
         setlist <<- c(setlist, list(data.frame(x = xseq, y = yseq, g = geese)))
         geese <<- geese + 1
-        
+
         data.frame(x = xseq, y = yseq, g = geese)
     })
     newseed <- reactive({
         input$doit
         myseed <<- myseed + 1
     })
-    
+
     output$hist <- renderPlot({
         axes()
-        
+
         x <- sample()
         d <- distfun()
-        
+
         d2 <- dplyr::bind_rows(setlist)
         d2 <- subset(d2, g >= geese - 10)
-        
+
         newseed()
-        
-        #histbreaks <- seq(min(floor(x)), max(ceiling(x)) + input$bins, by = input$bins)
-        
-        xmin <<- min(xmin, x-0.5, d$x-0.5)
-        xmax <<- max(xmax, x+0.5, d$x+0.5)
-        ymax <<- max(ymax, d$y*input$n, table(x))
-        
-        mytitle <- paste0("Histogram and PDF of ", input$dist)
-        
-        ggplot() + 
+
+
+        xmin <<- min(xmin, x - 0.5, d$x - 0.5)
+        xmax <<- max(xmax, x + 0.5, d$x + 0.5)
+        ymax <<- max(ymax, d$y * input$n, table(x))
+
+        mytitle <- paste0("Bar plot and PDF of ", input$dist)
+
+        ggplot() +
             geom_bar(aes(x = x), fill = "lightgrey", colour = 1) +
-            {if(nrow(d2) > nrow(d)) geom_step(data = d2, 
-                mapping = aes(x = x-0.5, y = y*input$n, 
-                    group = g, colour = geese - g, alpha = g), 
+            {if (nrow(d2) > nrow(d)) geom_step(data = d2,
+                mapping = aes(x = x - 0.5, y = y * input$n,
+                    group = g, colour = geese - g, alpha = g),
                 size = 0.6, na.rm = TRUE)} +
-            geom_step(data = d, mapping = aes(x = x-0.5, y = y*input$n),
+            geom_step(data = d, mapping = aes(x = x - 0.5, y = y * input$n),
                 colour = 2, size = 1) +
-            {if(nrow(d2) > nrow(d)) geom_segment(data = d2, 
-                mapping = aes(x = x - 0.5, xend = x - 0.5, 
-                    y = 0, yend = y*input$n, 
-                    group = g, colour = geese - g, alpha = g), 
-                size = 0.6, na.rm = TRUE)} + 
-            geom_segment(data = d, 
-                mapping = aes(x = x - 0.5, xend = x - 0.5, 
-                    y = 0, yend = y*input$n),
-                colour = 2, size = 1) + 
+            {if (nrow(d2) > nrow(d)) geom_segment(data = d2,
+                mapping = aes(x = x - 0.5, xend = x - 0.5,
+                    y = 0, yend = y * input$n,
+                    group = g, colour = geese - g, alpha = g),
+                size = 0.6, na.rm = TRUE)} +
+            geom_segment(data = d,
+                mapping = aes(x = x - 0.5, xend = x - 0.5,
+                    y = 0, yend = y * input$n),
+                colour = 2, size = 1) +
             theme_bw() +
             theme(legend.position = "none") +
-            coord_cartesian(xlim = c(xmin, xmax) + c(-0.5, 0.5), 
-                ylim = c(0, ymax))
-        
-        
-     #   for(i in 1:length(setlist)){
-     #       lines(setlist[[i]], col = rgb(0,0,0,0.1))
-     #   }
-        
-      #  lines(d, col = 4,lwd = 2)
+            coord_cartesian(xlim = c(xmin, xmax) + c(-0.5, 0.5),
+                ylim = c(0, ymax)) +
+            labs(title = mytitle)
+
+
+        #   for(i in 1:length(setlist)){
+        #       lines(setlist[[i]], col = rgb(0,0,0,0.1))
+        #   }
+
+        #  lines(d, col = 4,lwd = 2)
     }, res = 96)
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
