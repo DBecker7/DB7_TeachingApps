a4ac33317a0d0584fc008b3268af83448510668e
DBecker7 <d7becker@gmail.com>
2020-06-03 09:16:31 -0400

    Minor fixes, self-discovery qs, and p-values

--- Apps/nLarge/app.R
+++ Apps/nLarge/app.R
@@ -1,11 +1,4 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# nLarge
 
 library(shiny)
 library(ggplot2)
@@ -48,7 +41,12 @@ ui <- fluidPage(
         
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            plotOutput("distPlot"),
+            tags$div(HTML('Some questions:
+<ol><li>Why does this use the exponential distribution, rather than something closer to the normal distribution?</li>
+    <li>How large must n be before the normal approximation seems "reasonable"?</li>
+    <li>Comment on the bias of the normal approximation.</li>
+</ol>'))
         )
     )
 )
@@ -70,29 +68,46 @@ server <- function(input, output) {
             igseq <- dgamma(xseq, shape = n, rate = n*lam)
             nseq <- dnorm(xseq, 1/lam, 1/sqrt(n*lam^2))
             distdf <- data.frame(x = c(xseq, xseq), y = c(igseq, nseq), 
-                Distr. = rep(c("Actual Sampling Distribution", "Normal Approximation"), each = length(xseq)))
+                Distr. = rep(c("Actual Sampling Distr.", 
+                    "Normal Approximation"), 
+                    each = length(xseq)))
         } else {
-            minx <- min(qgamma(0.01, shape = n, rate = n*lam), qnorm(0.01, 1/lam, 1/sqrt(n*lam^2)))
-            maxx <- max(qgamma(0.99, shape = n, rate = n*lam), qnorm(0.99, 1/lam, 1/sqrt(n*lam^2)))
+            minx <- min(qgamma(0.01, shape = n, rate = n*lam), 
+                qnorm(0.01, 1/lam, 1/sqrt(n*lam^2)))
+            maxx <- max(qgamma(0.99, shape = n, rate = n*lam), 
+                qnorm(0.99, 1/lam, 1/sqrt(n*lam^2)))
             
             xseq2 <- seq(minx, maxx, length.out = 200)
             igseq <- dgamma(xseq2, shape = n, rate = n*lam)
             nseq <- dnorm(xseq2, 1/lam, 1/sqrt(n*lam^2))
             distdf <- data.frame(x = c(xseq2, xseq2), y = c(igseq, nseq), 
-                Distr. = rep(c("Actual Sampling Distribution", "Normal Approximation"), each = length(xseq2)))
+                Distr. = rep(c("Actual Sampling Distr.", 
+                    "Normal Approximation"), 
+                    each = length(xseq2)))
         }
+        
+        pval_gamma <- pgamma((1/lam - 1.96*1/sqrt(n*lam^2)), n, n*lam) +
+            pgamma((1/lam + 1.96*1/sqrt(n*lam^2)), n, n*lam, 
+                lower.tail = FALSE)
+        
+        subtit <- paste0("p_val from Normal approx = 0.05 \n", 
+            "p-val from actual sampling distr. = ", round(pval_gamma, 4))
         
         popdist <- ggplot() + 
             geom_line(aes(x = xseq, y = expseq)) +
             theme_bw() +
-            theme(title = element_text(size = 16), axis.text = element_text(size = 14)) +
+            theme(title = element_text(size = 16), 
+                axis.text = element_text(size = 14)) +
             labs(x = "Population Values", y = "Pop. Distribution")
         samplingdist <- ggplot(distdf) + 
             geom_line(aes(x = x, y = y, colour = Distr.), size = 1) +
             theme_bw() +
-            theme(title = element_text(size = 16), axis.text = element_text(size = 14), 
-                legend.text = element_text(size = 12)) +
+            theme(title = element_text(size = 16), 
+                axis.text = element_text(size = 14), 
+                legend.text = element_text(size = 12), 
+                legend.position = "right") +
             labs(x = "All Possible Means", y = "Sampling Distribution",
+                subtitle = subtit,
                 caption = "Created by Devan Becker\nGithub: DBecker7/DB7_TeachingApps")
         
         popdist / samplingdist
