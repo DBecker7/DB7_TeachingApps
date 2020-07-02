11365de281483b56976badca92267db111a01c69
DBecker7 <d7becker@gmail.com>
2020-07-02 09:53:29 -0400

    No more transparency

--- Tools/pnorm/app.R
+++ Tools/pnorm/app.R
@@ -4,16 +4,6 @@ library(shiny)
 library(ggplot2)
 
 ui <- fluidPage(
-    tags$style(
-        ".irs-bar {",
-        "  border-color: transparent;",
-        "  background-color: transparent;",
-        "}",
-        ".irs-bar-edge {",
-        "  border-color: transparent;",
-        "  background-color: transparent;",
-        "}"
-    ),
     
     # Application title
     titlePanel("Normal Probabilities"),
